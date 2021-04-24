51320e67e55ef09b7915fde92b34316dc00f456f
Abrahan Pulido <thehackeroffire@gmail.com>
2021-04-24 02:01:09 -0400

    #4 Define Routes in Web.php

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::get('posts', 'PostController@index');
+Route::group(['prefix' => 'post'], function () {
+    Route::post('add', 'PostController@store');
+    Route::get('edit/{id}', 'PostController@edit');
+    Route::post('update/{id}', 'PostController@update');
+    Route::delete('destroy/{id}', 'PostController@destroy');
 });
